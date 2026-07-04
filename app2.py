f67d697f6ddcf7464e70396b37e5793e2c121d87
Jiapei <yanjiapei99@outlook.com>
2026-07-04 22:09:37 +0800

    Add files via upload

--- app2.py
+++ app2.py
@@ -43,7 +43,7 @@ def get_dataset_path():
     # Get the current script directory
     current_dir = os.path.dirname(os.path.abspath(__file__))
     # Construct the path to the CSV file
-    csv_path = os.path.join(current_dir, "data", "customer_reviews.csv")
+    csv_path = os.path.join(current_dir, "customer_reviews.csv")
     return csv_path
 
 #add a slider for temperature
